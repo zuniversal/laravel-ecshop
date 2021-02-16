e6f17702caf8c0c496cfcdd4938ebb185d1a620d
Administrator <admin@example.com>
2021-02-17 00:09:47 +0800

    learn 3-7 数据库操作-原生sql - 原生sql操作数据库

--- app/Http/Controllers/LearnController.php
+++ app/Http/Controllers/LearnController.php
@@ -58,7 +58,24 @@ class LearnController extends Controller
     // 3-6
     public function dbTest()
     {
-        DB::select('select * from user');
+        // $res = DB::select('select * from users');
+        // $res = DB::select('select * from users where name = "zyb2"');
+
+        // ？ 绑定 将n个参数按顺序放入 
+        // $res = DB::select('select * from users where id = ?', [ 1, ]);
+
+        // 参数值变量绑定 这样可以不考虑顺序
+        $res = DB::select('select * from users where id = :id', [ 'id' => 1, ]);
+        // dd($res[0]);
+        // dd($res);
+
+        // $res = DB::insert('insert into users (name, email, password) values (?, ?, ?)', [ 'zyb3', '604688486@qq.com', 8, ]);
+        // $res = DB::update('update users set email = ? where id = ?', [ '666@qq.com', 1, ]);
+        // $res = DB::statement('drop table users');// 不推荐这样写
+        // $res = DB::statement('drop table users');// 不推荐这样写 因为这样写直接将表结构删除了 
+        dd($res);
+
+        return $res; 
         return 'dbTest';
     }
 
