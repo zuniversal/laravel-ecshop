b5d1228c4ff1f9188d393e4013527f558a1839ab
Administrator <admin@example.com>
2021-02-17 08:39:33 +0800

    learn 3-8 数据库操作-获取查询结果 - 获取列 数组 字段 分页 聚合函数 存在函数的使用

--- app/Http/Controllers/LearnController.php
+++ app/Http/Controllers/LearnController.php
@@ -58,6 +58,7 @@ class LearnController extends Controller
     // 3-6
     public function dbTest()
     {
+        // 3-7
         // $res = DB::select('select * from users');
         // $res = DB::select('select * from users where name = "zyb2"');
 
@@ -65,7 +66,7 @@ class LearnController extends Controller
         // $res = DB::select('select * from users where id = ?', [ 1, ]);
 
         // 参数值变量绑定 这样可以不考虑顺序
-        $res = DB::select('select * from users where id = :id', [ 'id' => 1, ]);
+        // $res = DB::select('select * from users where id = :id', [ 'id' => 1, ]);
         // dd($res[0]);
         // dd($res);
 
@@ -73,6 +74,28 @@ class LearnController extends Controller
         // $res = DB::update('update users set email = ? where id = ?', [ '666@qq.com', 1, ]);
         // $res = DB::statement('drop table users');// 不推荐这样写
         // $res = DB::statement('drop table users');// 不推荐这样写 因为这样写直接将表结构删除了 
+
+        // 3-8
+        // 方法返回的是一个 Illuminate\Support\Collection  集合对象 里面有个 items 属性 里面就是想要的结果 
+        // 集合只是对php数组进行封装 增加了一些数组相关的操作函数使用 
+        // $res = DB::table('users')->where([ 'id' => 1, ])->get();
+        // $res = DB::table('users')->find(1);// 查询的结果就是一个对象
+        $res = DB::table('users')->where([ 'id' => 2, ])->first();// 返回 查询的第一个对象结果
+        $res = DB::table('users')->where([ 'id' => 2, ])->value('name');// 只需要其中一个值结果
+        $res = DB::table('users')->pluck('name');// 获取一列数据
+        $res = DB::table('users')->pluck('name')->toArray();// 转化为数组
+        $res = DB::table('users')->paginate(2);// 分页
+        $res = DB::table('users')->simplePaginate(2);// 唯一的区别就是少了 total 字段 没有统计数据库里该字段数量
+        $res = DB::table('users')->max('id');
+        $res = DB::table('users')->min('id');
+        $res = DB::table('users')->avg('id');// "2.0000"
+        $res = DB::table('users')->count('id');
+        $res = DB::table('users')->sum('id');
+        $res = DB::table('users')->where('id', 4)->exists();
+        $res = DB::table('users')->where('id', 4)->doesntExist();
+
+
+        
         dd($res);
 
         return $res; 
